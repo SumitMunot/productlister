1372507729da411d90b5c23e0ed57350b4eef5dc
Sumit Munot <ersumitmunot@gmail.com>
2017-06-24 13:38:57 +0530

    Added type to the product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,30 @@ class Product < ApplicationRecord
   def price_sum(product_type)
   	Product.where(type: product_type).sum { |prod| prod.price }
   end
+
+  def self.new_by_type(attributes)
+    type = attributes.delete(:type)
+    klass = if %w(Pen Notebook).include?(type)
+      type.constantize
+    else
+      Product
+    end
+    klass.new(attributes)
+  end
+
+  def self.type
+    "Generic"
+  end
+
+  def type
+    self.class.type
+  end
+
+  def pen?
+    false
+  end
+
+  def notebook?
+    false
+  end
 end
